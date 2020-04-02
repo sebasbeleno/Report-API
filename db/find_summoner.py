b0ab91b0c82c57847357bff028b29e3823588d2b
Sebastian Beleño <sebasbeleno15@gmail.com>
2020-04-02 03:01:07 -0500

    [BUG] Guardado de nombre en la base de datos.
    Ahora, al momento de guardar el summonerName en la base de datos,
    Primero convierte este a lowercase.
    
    Esto para evitar nombres con diferentes case's y guardar invocadores
    por error.git

--- db/find_summoner.py
+++ db/find_summoner.py
@@ -11,4 +11,7 @@ def find_summuner(summoner_name):
         Esta función bsca en la base de datos
         el invocador que sea enviado por parametro.
     """
+
+    summoner_name = summoner_name.lower()
+
     return SUMMONER_COLLECTION.find_one({'name': summoner_name})
